870533709ad45dc97a0420de434161742e4495b6
pit1988 <gabelli29@hotmail.com>
2016-12-13 10:52:14 +0100

    Completati esercizi cap.3

--- Cap3/3.11.8.java
+++ Cap3/3.11.8.java
@@ -9,17 +9,17 @@ class A{
 
 class B extends A{
 	public void m2() {print("B.m2");}
-	public void m3() {print("B.m3");
+	public void m3() {print("B.m3"); }
 }
 
 class C extends A{
-	public void m1() {print("C.m1");}
-	public void m2() {print("C.m2");
+	public void m1() {print("C.m1"); }
+	public void m2() {print("C.m2"); }
 }
 
 class D extends C{
 	public void m1() {super.m1(); print("D.m1");}
-	public void m3() {print("D.m3");
+	public void m3() {print("D.m3");  }
 }
 
 class Esercizio{
@@ -33,23 +33,23 @@ class Esercizio{
 		/*
 			Per ognuna delle seguenti istruzioni si dica se compila, se la sua esecuzione provoca un'eccezione o se l'istruzione compila correttamente senza lanciare eccezioni
 		*/
-		ref1.m1(); 
-		ref2.m1();
-		ref4.m1();
-		ref5.m1(); //fine 1 riga
-		ref6.m1();
-		ref1.m2();
-		ref2.m2();
-		ref3.m2(); //fine 2 riga
-		ref4.m2();
-		ref6.m2();
-		ref5.m3(); //fine 3 riga
-		((B)ref1).m3();
-		((D)ref4).m3();
-		((D)ref5).m3();
-		((B)ref2).m3(); //fine 4 riga
-		((C)ref2).m2();
-		((D)ref6).m2();
+ 		ref1.m1(); //stampa A.m1, B.m2
+		ref2.m1(); //stampa C.m1 D.m1
+		ref4.m1(); //stampa C.m1 
+		ref5.m1(); //C.m1 D.m1 
+		ref6.m1(); //Non compila: Object non ha il metodo m1. 
+		ref1.m2(); //B.m2 
+		ref2.m2(); //C.m2 
+		ref3.m2(); //B.m2 
+		ref4.m2(); //C.m2 
+		ref6.m2(); //non compila (object non ha m2) 
+		ref5.m3(); //non compila (C non ha m3)
+		((B)ref1).m3(); //La conversione ha successo. b.m3 
+		((D)ref4).m3(); //Non compila: non posso fare cast al sottotipo D per un'oggetto C 
+		((D)ref5).m3(); //D.m3 
+		((B)ref2).m3(); //Compila, ma Solleva classCastException (B non sta nella gerarchia di D) 
+		((C)ref2).m2(); //C.m2 
+		((D)ref6).m2(); //C.m2
 	}
 }
 
