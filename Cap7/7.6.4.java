ccf2ea2d1cf15b088cc162cc1c2bbd06b204c06e
pit1988 <gabelli29@hotmail.com>
2016-12-19 00:26:41 +0100

    Concluso es. 7.6.4

--- Cap7/7.6.4.java
+++ Cap7/7.6.4.java
@@ -1,15 +1,34 @@
 /*Modificare la seguente classe in modo tale che definisca il tipo di un conto corrente bancario che può correttamente essere gestito da più thread concorrenti*/
 
-
+import java.util.concurrent.locks.*;
 
 class ContoBancario{
+	Lock lock = new ReentrantLock();
 	private int num;
 	private int saldo;
 	ContoBancario(int n, int s) {num = n; saldo = s;}
 	public int getSaldo() {return saldo;}
-	public void deposita(int n) {saldo = saldo + n;}
-	public void preleva(int n) {saldo = saldo - n;}
+	public void deposita(int n) {
+		lock.lock();
+		try{
+			saldo = saldo + n;
+		} catch(InterruptedException e) {e.printStackTrace();}
+		finally{
+			lock.unlock();
+		}
+	}
+	public void preleva(int n) {
+		lock.lock();
+		try{
+			saldo = saldo - n;
+		} catch(InterruptedException e) {e.printStackTrace();}
+		finally{
+			lock.unlock();
+		}
+	}
 	public void trasferisci(int n, ContoBancario dest) {
-		preleva(n); dest.deposita(n);
+		preleva(n);
+		dest.deposita(n);
 	}
 }
+
