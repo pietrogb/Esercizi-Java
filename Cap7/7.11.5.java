71afb0c072eb9d6420436eb4d9ad3611b3abc58e
pit1988 <gabelli29@hotmail.com>
2017-01-06 09:51:40 +0100

    Concluso es.5 cap. 7

--- Cap7/7.11.5.java
+++ Cap7/7.11.5.java
@@ -2,8 +2,8 @@
 Definire una Gui composta da un pannello e tre vottoni etichettati rispettivamente Avvia, Pausa e Stop, la cui pressione soddisfi le sewguenti specifiche.
 	- La pressione del tasto Avvia deve avviare un thread che ogni 30 millisecondi cambia il colore del pannello di sfondo. Se l'esecuzione del thread era stata sospesa, la pressione del tasto Avvia deve riattivare l'esecuzione del thread.
 	- la pressione del tasto Pausa deve causare la sospensione dell'esecuzione del thread che fa cambiare il colore al pannello di sfondo. La pressione di questo tasto non deve causare attesa attiva.
-	- la pressione del tasto Stop deve impostare lo stato di interruzione del thread che fa cambiare il colore al pannello di sfondo. Una volta interrotto, il thread deve terminare la sua esecuizone e non deve essere riattivabile con una successiva pressione del tasto Avvia
-	- si ricorda che il metodo public void setBackbround(Color c)  della classe Component imposta a c il colore delllo sfondo del componente d'invocazione
+	- la pressione del tasto Stop deve impostare lo stato di interruzione del thread che fa cambiare il colore al pannello di sfondo. Una volta interrotto, il thread deve terminare la sua esecuzione e non deve essere riattivabile con una successiva pressione del tasto Avvia
+	- si ricorda che il metodo public void setBackbround(Color c)  della classe Component imposta a c il colore dello sfondo del componente d'invocazione
 	- si ricorda che il metodo public void interrupt() imposta a true lo stato di interruzione del thread di invocazione. Inoltre, se al momento d'invocazione t.interrupt() il tread t è bloccato in una chiamata di un metodo sleep o wait, allora t riceve una InterruptException ed il suo stato di interruzione viene reimpostato a false.
 	- si ricorda che il metodo public boolead isInterrupted() testa se lo stato di interruzione di un thread è true
 */
@@ -36,6 +36,8 @@ class ButtonFrame extends JFrame {
 
 class ButtonPanel extends JPanel {
 	T t = null;
+	private Object o = new Object();
+
 	private JButton avvia = new JButton("Avvia");
 	private JButton pausa = new JButton("Pausa");
 	private JButton stop = new JButton("Stop");
@@ -48,22 +50,44 @@ class ButtonPanel extends JPanel {
 		private boolean runFlag = true;
 		T() {start();}
 
-		void invertiFlag() {runFlag = !runFlag; }
+		public void invertiFlag() {runFlag = !runFlag;}
 
+		public boolean isRunning() {return runFlag;}
 		public void run() {
 			while(true) {
-				try { sleep(299);}
-				catch(InterruptedException e) {
-					System.err.println("Interrupted");
+				synchronized(o){
+					while(runFlag){
+						setBackground(randomColor());
+						try { this.sleep(299);
+						} catch(InterruptedException e) {
+							interrupt();
+							System.err.println(isInterrupted() + "Thread interrotto durante lo sleep");
+							return;
+						}
+						/*try{
+							o.wait();
+						} catch(InterruptedException e) {
+							interrupt();
+							System.err.println(isInterrupted() + "Thread interrotto durante la wait");
+							return;
+						}*/
+					}
 				}
-				if(runFlag && !isInterrupted()) setBackground(randomColor());
 			}
 		}
 	}
 
 	class Lstart implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(t == null) t = new T();
+			if(t == null) {
+				t = new T();
+			}
+			else if (!t.isInterrupted() && !(t.isRunning())) {
+				synchronized(o) {
+					t.invertiFlag();
+					o.notify();
+				}
+			}
 		}
 	}
 
@@ -76,11 +100,7 @@ class ButtonPanel extends JPanel {
 
 	class Lstop implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(t != null && !(t.isInterrupted())) {
-				t.invertiFlag();
-				t.notifyAll();
-				t.interrupt();
-			}
+			t.interrupt();
 		}
 	}
 
