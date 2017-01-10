1ab364cc242fdcd10e3373e3bae5f8c8bb6b4aa7
pit1988 <gabelli29@hotmail.com>
2017-01-10 16:54:41 +0100

    concluso es.14

--- Cap7/7.11.14.java
+++ Cap7/7.11.14.java
@@ -1,16 +1,21 @@
 // Generatore di numeri casuali
 
-public class C{
+ class C{
 	private int y=0;
+	boolean goPrint = true;
 	public synchronized void incrementa() {
 		if(0<=y && y<255)
 			y++;
 		else y=0;
 	}
-	// ...
+
+	public synchronized void print() {
+		System.out.println(" " + y%100 + " ");
+		notifyAll();
+	}
 }
 
-public class T1 extends Thread {
+ class T1 extends Thread {
 	private C c;
 	public T1(C r) {c = r;}
 	public void run() {
@@ -22,11 +27,19 @@ public class T1 extends Thread {
 	}
 }
 
-public class T2 extends Thread {
+ class T2 extends Thread {
 	private C c;
 	public T2(C r) {c = r;}
 	public void run() {
-		// ...
+		while(true){
+			try{
+				wait();
+			} catch(Exception e) {}
+			c.print();
+			try{
+				wait();
+			} catch(Exception e) {}
+		}
 	}
 
 }
@@ -38,3 +51,12 @@ public class Ex {
 		t1.start(); t2.start();
 	}
 }
+
+/*
+Definire il metodo run() di t2 in modo che le classi compilino correttamente e l'esecuzione di Ex provochi la stampa di una sequenza infinita di numeri tutti compresi tra 0 e 99 in cui ogni numero è diverso dal successivo.
+La definizione di run() di T2 deve rispettare il seguente vincolo:	
+	Sono permesse solamente invocazioni di metodi della classe C esclusi i costruttori; qundi un'invocazione System.out.print(..) non è permessa
+Definire gl'opportuni operatori della classe C che si ritengono necessari per la definizione della run() di T2. Non ci sono restrizioni sui metodi della classe C.
+*/
+
+// 7.11.14
